7bca33ecf8d497878e56c9d7a85de5d2a23aeb2e
unknown <nikakotov2005@gmail.com>
2025-06-10 21:57:07 +0300

    imports/exports in nestJS

---
+++ src/movie/movie.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class MovieService {
+  constructor(private readonly userService: UserService) {}
+  test() {
+    return this.userService.test();
+  }
+}
